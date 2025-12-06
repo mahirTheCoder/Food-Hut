90d3f44f99806a99f5c75d71cf56322bc2f71076
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-12-06 22:20:24 +0600

    logo

---
+++ src/app/Componets/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className=" text-black py-6">
+      <div className=" container flex justify-between items-center px-4">
+        {/* Logo */}
+        <h1 className="text-3xl font-bold tracking-wide">
+          Food<span className="text-[#F65F5F]">Hut</span>
+        </h1>
+
+   
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
