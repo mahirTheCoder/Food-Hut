0eb8a01b7b23b3050124754b628dd6bca0548b7f
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-12-08 17:38:07 +0600

    cards 3

--- src/app/Componets/Offers.jsx
+++ src/app/Componets/Offers.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import offer from "../../../public/offers.png";
 import offer4 from "../../../public/offer4.png";
+import offer3 from "../../../public/offer3.png";
 import ratig from "../../../public/rating.png";
 import Image from "next/image";
 
@@ -53,6 +54,35 @@ const Offers = () => {
 
           
           {/* ------- Card 2 ------- */}
+          <div className=" bg-linear-to-b from-gray-500 to-amber-800 w-77 h-92 rounded-2xl  shadow-xl  transition flex flex-col items-center relative mt-[66px]">
+            {/* ----img part-------- */}
+            <div className="img absolute top-[-30%]">
+              <Image src={offer3} alt="offer" className="w-63 h-63  " />
+            </div>
+
+            {/* ------rationg part-------- */}
+            <div className="flex items-center justify-center gap-2 mt-34">
+              <p className="flex items-center gap-1 text-[#F65F5F] text-sm">
+                <Image src={ratig} alt="offer" />★ 4.2
+              </p>
+            </div>
+
+            <h3 className=" text-[#F65F5F]  font-semibold text-lg mt-7.5">
+             Desi Chowmein
+            </h3>
+            <p className="text-center w-[243px] text-black text-sm mt-4">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry
+            </p>
+
+            <button className="bg-[#F65F5F] text-white mt-7.5 px-5 py-3 rounded-full hover:bg-[#ff7979] transition">
+              Order Now
+            </button>
+          </div>
+
+
+          
+          {/* ------- Card 3 ------- */}
           <div className=" bg-linear-to-b from-gray-500 to-amber-800 w-77 h-92 rounded-2xl  shadow-xl  transition flex flex-col items-center relative mt-[66px]">
             {/* ----img part-------- */}
             <div className="img absolute top-[-30%]">
