380eee2a18f5095a0a87dfa1d0a245861a0e45bc
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-12-08 17:53:09 +0600

    3th card complete

--- src/app/Componets/Offers.jsx
+++ src/app/Componets/Offers.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import offer from "../../../public/offers.png";
 import offer4 from "../../../public/offer4.png";
 import offer3 from "../../../public/offer3.png";
+import offer2 from "../../../public/offer2.png";
 import ratig from "../../../public/rating.png";
 import Image from "next/image";
 
@@ -22,7 +23,7 @@ const Offers = () => {
         </div>
 
         {/* Cards Row */}
-        <div className="flex justify-between flex-wrap">
+        <div className="flex justify-between flex-wrap lg:mt-33 gap-30 lg:gap-0">
 
 
           {/* ------- Card 1 ------- */}
@@ -35,7 +36,7 @@ const Offers = () => {
             {/* ------rationg part-------- */}
             <div className="flex items-center justify-center gap-2 mt-34">
               <p className="flex items-center gap-1 text-[#F65F5F] text-sm">
-                <Image src={ratig} alt="offer" />★ 4.5
+                <Image src={ratig} alt="offer" /> <span className="text-2xl">★ 4 . 5</span>
               </p>
             </div>
 
@@ -53,7 +54,35 @@ const Offers = () => {
           </div>
 
           
-          {/* ------- Card 2 ------- */}
+          {/* ------- Card 3 ------- */}
+          <div className=" bg-linear-to-b from-gray-500 to-amber-800 w-77 h-92 rounded-2xl  shadow-xl  transition flex flex-col items-center relative mt-[66px]">
+            {/* ----img part-------- */}
+            <div className="img absolute top-[-30%]">
+              <Image src={offer2} alt="offer" className=" w-63 h-63  " />
+            </div>
+
+            {/* ------rationg part-------- */}
+            <div className="flex items-center justify-center gap-2 mt-34">
+              <p className="flex items-center gap-1 text-[#F65F5F] text-sm">
+                <Image src={ratig} alt="offer" /> <span className="text-2xl">★ 4 . 8</span>
+              </p>
+            </div>
+
+            <h3 className=" text-[#F65F5F]  font-semibold text-lg mt-7.5">
+           Chicken Tikka
+            </h3>
+            <p className="text-center w-[243px] text-black text-sm mt-4">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry
+            </p>
+
+            <button className="bg-[#F65F5F] text-white mt-7.5 px-5 py-3 rounded-full hover:bg-[#ff7979] transition">
+              Order Now
+            </button>
+          </div>
+
+
+          {/* ------- Card 3 ------- */}
           <div className=" bg-linear-to-b from-gray-500 to-amber-800 w-77 h-92 rounded-2xl  shadow-xl  transition flex flex-col items-center relative mt-[66px]">
             {/* ----img part-------- */}
             <div className="img absolute top-[-30%]">
@@ -63,7 +92,7 @@ const Offers = () => {
             {/* ------rationg part-------- */}
             <div className="flex items-center justify-center gap-2 mt-34">
               <p className="flex items-center gap-1 text-[#F65F5F] text-sm">
-                <Image src={ratig} alt="offer" />★ 4.2
+               <Image src={ratig} alt="offer" /> <span className="text-2xl">★ 4 . 2</span>
               </p>
             </div>
 
@@ -82,7 +111,7 @@ const Offers = () => {
 
 
           
-          {/* ------- Card 3 ------- */}
+          {/* ------- Card 4 ------- */}
           <div className=" bg-linear-to-b from-gray-500 to-amber-800 w-77 h-92 rounded-2xl  shadow-xl  transition flex flex-col items-center relative mt-[66px]">
             {/* ----img part-------- */}
             <div className="img absolute top-[-30%]">
@@ -92,7 +121,7 @@ const Offers = () => {
             {/* ------rationg part-------- */}
             <div className="flex items-center justify-center gap-2 mt-34">
               <p className="flex items-center gap-1 text-[#F65F5F] text-sm">
-                <Image src={ratig} alt="offer" />★ 5.0
+               <Image src={ratig} alt="offer" /> <span className="text-2xl">★ 5 . 0</span>
               </p>
             </div>
 
